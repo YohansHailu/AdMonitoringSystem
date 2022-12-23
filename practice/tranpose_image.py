411813af08b14708851d89cbe2e54707e27b81be
Yohans Hailu <95067604+YohansHailu@users.noreply.github.com>
2022-12-23 22:29:52 +0300

    Update tranpose_image.py

--- practice/tranpose_image.py
+++ practice/tranpose_image.py
@@ -4,3 +4,7 @@ roadImg = cv2.imread("./road.jpg")
 
 roadImg = roadImg.transpose((1,0,2))
 cv2.imwrite("read_copy.jpg", roadImg)
+
+#adaptive threshold
+roadImg = cv2.adaptiveThreshold(roadImg, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 3)
+cv2.imwrite("adaptive_read_copy.jpg", roadImg)
